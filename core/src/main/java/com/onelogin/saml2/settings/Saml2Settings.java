0f11aa7f6fa0173fdf6c11a689a0b908f8771473
Christopher Schommer <c.schommer@resolution.de>
2022-10-24 09:09:54 +0000

    Merged in feature/SSSO-1717-make-audience-check-configurab (pull request #2)
    Make audience check configurable
    
    
    Approved-by: Oliver Simon

--- core/src/main/java/com/onelogin/saml2/settings/Saml2Settings.java
+++ core/src/main/java/com/onelogin/saml2/settings/Saml2Settings.java
@@ -77,6 +77,7 @@ public class Saml2Settings {
 	private boolean validateTimestamps = true;
 	private boolean validateDestination = true;
 	private boolean validateRecipient = true;
+	private boolean validateAudience = true;
 
 	private String requestedAuthnContextComparison = "exact";
 	private boolean wantXMLValidation = true;
@@ -100,7 +101,7 @@ public class Saml2Settings {
 	private Organization organization = null;
 
 	private boolean spValidationOnly = false;
-	
+
 	/**
 	 * @return the strict setting value
 	 */
@@ -971,6 +972,14 @@ public class Saml2Settings {
 
 	public boolean isValidateDestination() {
 		return this.validateDestination;
+	}
+
+	public void setValidateAudience(boolean validateAudience) {
+		this.validateAudience = validateAudience;
+	}
+
+	public boolean isValidateAudience() {
+		return this.validateAudience;
 	}
 
 	public boolean isValidateRecipient() {
